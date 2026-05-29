c8b1190ec46c989be47aaa73f84cfd21b47273a3
Emyleaf <39247685+Emyleaf@users.noreply.github.com>
2026-05-30 00:15:23 +0200

    v.0

--- scripts/playerscripts/state_walk.gd
+++ scripts/playerscripts/state_walk.gd
@@ -1,6 +1,6 @@
 class_name State_Walk extends State
 
-@export var move_speed : float = 500.0
+@export var move_speed : float = 200.0
 
 @onready var idle : State = $"../Idle"
 @onready var attack : State = $"../Attack"
