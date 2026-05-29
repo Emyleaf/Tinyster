6e6e41ee1bbe44eb2f8ca2c01cc6e311ed602532
Emyleaf <39247685+Emyleaf@users.noreply.github.com>
2026-05-29 23:42:29 +0200

    v.0

--- scripts/playerscripts/player.gd
+++ scripts/playerscripts/player.gd
@@ -6,6 +6,7 @@ class_name Player extends CharacterBody2D
 @onready var state_machine : PlayerStateMachine = $StateMachine
 
 var direction : Vector2 = Vector2.ZERO
+var cardinal_direction : Vector2 = Vector2.ZERO
 
 signal DirectionChanged(new_direction : Vector2)
 
@@ -23,13 +24,23 @@ func UpdateAnimation(state : String) -> void:
 	pass
 
 func SetDirection() -> bool:
-	var new_dir : float = sign(direction.x)
-	if new_dir == 0:
+	if direction == Vector2.ZERO:
 		return false
-	sprite.flip_h = new_dir < 0
-	DirectionChanged.emit(direction)
+
+	var new_dir : Vector2 = cardinal_direction
+	if direction.y == 0:
+		new_dir = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
+	elif direction.x == 0:
+		new_dir = Vector2.UP if direction.y < 0 else Vector2.DOWN
+
+	if new_dir == cardinal_direction:
+		return false
+
+	cardinal_direction = new_dir
+	DirectionChanged.emit(new_dir)
+	sprite.flip_h = (cardinal_direction == Vector2.LEFT)
 	return true
-	
+
 func AnimDirection() -> String:
 	if direction.y < 0:
 		return "up"
--- scripts/playerscripts/player_interactions_host.gd
+++ scripts/playerscripts/player_interactions_host.gd
@@ -9,13 +9,13 @@ func _ready() -> void:
 
 func UpdateDirection(dir : Vector2) -> void:
 	match dir:
-		Vector2(0,-1):
+		Vector2.DOWN:
 			rotation_degrees = 0
-		Vector2(0,1):
+		Vector2.UP:
 			rotation_degrees = 180
-		Vector2(-1,0):
+		Vector2.LEFT:
 			rotation_degrees = 90
-		Vector2(1,0):
+		Vector2.RIGHT:
 			rotation_degrees = -90
 		_:
 			rotation_degrees = 0
