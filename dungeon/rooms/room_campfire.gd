4d9767eaeb6bd594400cde44d2f578c873a18fcf
Emyleaf <39247685+Emyleaf@users.noreply.github.com>
2026-07-24 14:37:03 +0200

    campfire

--- dungeon/rooms/room_campfire.gd
+++ dungeon/rooms/room_campfire.gd
@@ -1,24 +1,15 @@
 extends Node2D
 
-var doors_to_open : Array[Door] = []
-
 @onready var tilemap: TileMapLayer = $TileMapLayer
-@onready var door_north: Door = $DoorN
-@onready var door_south: Door = $DoorS
-@onready var door_east: Door = $DoorE
-@onready var trans_east: LevelTransition = $TransE
-@onready var spawn_from_west: Marker2D = $SpawnFromWest
+@onready var spawnpoint: Marker2D = $Spawnpoint
 
 
 func _ready():
 	_position_player_on_entry()
-	
-	if DungeonManager.last_room:
-		print("Stanza - Riga: %d, Colonna: %d" % [DungeonManager.last_room.column+1, DungeonManager.last_room.row])
 
 
 func _position_player_on_entry() -> void:
-	var spawn_point: Marker2D = spawn_from_west
+	var spawn_point: Marker2D = spawnpoint
 			
 	if PlayerManager.player == null:
 		PlayerManager.player = PlayerManager.PLAYER.instantiate()
